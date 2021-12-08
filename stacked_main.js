b253bc72a2dc4a799ad4c30539041d4332f665d0
Jaimie Jin <jinjaimie12@gmail.com>
2021-12-07 17:15:24 -0800

    stacked_main data change

--- stacked_main.js
+++ stacked_main.js
@@ -13,14 +13,25 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // Parse the Data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_stacked.csv", function(data) {
+d3.csv("processed_data/id_food_prod.csv", function(data) {
     console.log(data);
   // List of subgroups = header of the csv files = soil condition here
-  var subgroups = data.columns.slice(1)
-  console.log(subgroups)
+  // var subgroups = data.columns.slice(1)
+  // console.log(subgroups)
+
+  var nested = d3.nest()
+  .key(function(d) {
+    return d.category;
+  })
+  .rollup(function(leaves) {
+    return {food: leaves};
+  })
+  .entries(data);
+
+console.log(nested);
 
   // List of groups = species here = value of the first column called group -> I show them on the X axis
-  var groups = d3.map(data, function(d){return(d.group)}).keys()
+  var groups = d3.map(data, function(d){return(d.category)}).keys()
   console.log(groups)
 
   // Add X axis
@@ -38,30 +49,38 @@ d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/da
     .range([ height, 0 ]);
   svg.append("g")
     .call(d3.axisLeft(y));
-
   // color palette = one color per subgroup
-  var color = d3.scaleOrdinal()
-    .domain(subgroups)
-    .range(['#e41a1c','#377eb8','#4daf4a'])
+  let cat = nested.map(n => n.key);
+  var color = d3.scaleOrdinal(d3.schemeCategory10)
+    // .domain(cat)
+    // .range(['#e41a1c','#377eb8','#4daf4a', '#e41a1c','#377eb8','#4daf4a', '#e41a1c','#377eb8','#4daf4a', '#e41a1c','#377eb8','#4daf4a'])
 
   //stack the data? --> stack per subgroup
-  var stackedData = d3.stack()
-    .keys(subgroups)
-    (data)
+  // var stackedData = d3.stack()
+  //   .keys(nested.food)
+  //   (data)
 
   // Show the bars
   svg.append("g")
     .selectAll("g")
     // Enter in the stack data = loop key per key = group per group
-    .data(stackedData)
+    .data(nested)
     .enter().append("g")
       .attr("fill", function(d) { return color(d.key); })
       .selectAll("rect")
       // enter a second time = loop subgroup per subgroup to add all rectangles
-      .data(function(d) { return d; })
+      .data(function(d) { console.log(d.value.food); return d.value.food; })
       .enter().append("rect")
-        .attr("x", function(d) { return x(d.data.group); })
-        .attr("y", function(d) { return y(d[1]); })
+        .attr("x", function(d,i) { return i  })
+        .attr("y", function(d) { if(d.Total_emission !== NaN) { return y(parseFloat(d.Total_emission)); } else {return y(0)} })
         .attr("height", function(d) { return y(d[0]) - y(d[1]); })
         .attr("width",x.bandwidth())
 })
+
+function dataPreprocessor(row) {
+  return {
+      category: row.category,
+      foodName: row['Food product'],
+      emissionTot: parseFloat(row.Total_emission)
+  };
+}
